066ce660d7a34737b724b0166a3ac7ca93628efb
Konrad Bartczak <konradbartczak@onet.pl>
2019-01-16 19:29:42 +0100

    init commit

---
+++ src/main/java/com/konrad/bootstrap_course/BootstrapCourseApplication.java
@@ -0,0 +1,14 @@
+package com.konrad.bootstrap_course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapCourseApplication.class, args);
+    }
+
+}
+
